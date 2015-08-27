de3a06263735a496f670377fcd19c47912dc26d5
masimo <shok.max@gmail.com>
2015-08-27 14:14:58 +0300

    added htaccess and emplate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
 <div class="main-box">
 	<div class="container">
 		<div class="container-fluid">
-			<!-- <h1>прокладывай свой туристический путь <br> вместе с My way</h1> -->
+			<h1>прокладывай свой туристический путь <br> вместе с My way</h1>
 			<div class="input-search-box">
 				<div class="input-text-box">
 					<input type="text" class="search-field" placeholder='Введите текст'>	
@@ -16,15 +16,15 @@
 				<input type="button" class="submit-search"></input>
 			</div>
 			<div class="main-button-box">
-				<!-- <a class="top-route">Популярные маршруты</a>
-				<a class="latest-route">Последние маршруты</a> -->
+				<a class="top-route">Популярные маршруты</a>
+				<a class="latest-route">Последние маршруты</a>
 			</div>
 		</div>
 	</div>
 	<section class="main-body-container">
-		<!-- <h2 class="title-road">
+		<h2 class="title-road">
 			Новые маршруты
-		</h2> -->
+		</h2>
 		<div class="grid-items"></div>
 		<div class="places-outer-box"></div>
 	</section>
