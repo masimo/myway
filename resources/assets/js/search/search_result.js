87ba0076de21821677b4d3aa4da3d14f1824feb5
masimo <shok.max@gmail.com>
2015-11-04 14:39:22 +0200

    #21 creating search result page js

---
+++ resources/assets/js/search/search_result.js
@@ -0,0 +1,8 @@
+require(['C.drop-down.widget', 'C.search-result.widget'], function(dropDown, searchResult) {
+	var View = Backbone.View.extend({
+		initialize: function() {
+			//
+		}
+	})
+	return new View();
+});
